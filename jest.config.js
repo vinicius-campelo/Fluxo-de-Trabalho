045a7014c7f2a06059d87ec6c3b20fac700f5ac3
vinicius campelo <autanbr@gmail.com>
2024-08-20 23:27:22 -0300

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+// jest.config.js
+module.exports = {
+  testMatch: [
+    '**/__tests__/**/*.[jt]s?(x)',
+    '**/?(*.)+(spec|test).[tj]s?(x)'
+  ],
+  testPathIgnorePatterns: ['/node_modules/'],
+};
